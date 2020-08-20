403d12b8426396c0bd16dbc90344d0337206c576
monitor <monitor@monitor.lowski.dev>
2020-08-19 20:49:58 -0400

    more automation

---
+++ setup-debian.sh
@@ -0,0 +1,10 @@
+
+cd ~/ &&
+mkdir cardano &&
+cd cardano &&
+pkg update -y && pkg install curl proot tar wget -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian.sh | bash &&
+cd ~/cardano/debian-fs/root/ &&
+mkdir node &&
+cd node &&
+wget https://lowski.dev/cardano-android.tar.gz &&
+tar xvf cardano-android.tar.gz
--- setup.sh
+++
@@ -1,6 +0,0 @@
-
-cd ~/ &&
-mkdir cardano &&
-cd cardano &&
-pkg update -y && pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian.sh | bash &&
-./startDebian.sh
